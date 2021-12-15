5bff6e4b06817ccfed242bd659aa42ba83e91354
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-15 13:42:29 +0530

    sweet: add missing apn configs for Android S Some roms need APN configs to add APNs automatically on first boot, so that users don't have to add APNs in settings. Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # ANXCamera
 $(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
