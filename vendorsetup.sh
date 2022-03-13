588e0bc8d13cba430a5ac26979201592850f6c6f
zaidkhan0997 <kzaid0997@gmail.com>
2022-03-13 16:26:58 +0530

    sweet: track los display hal
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/common
 
 # clone
-git clone -b twelve https://github.com/zaidkhan0997/hardware_qcom-caf_sm8150-common_display.git hardware/qcom-caf/sm8150/display
+git clone -b 19.1-sm8150 https://github.com/zaidkhan0997/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
 git clone -b pex https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
 git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
