simple-sunrise/device_xiaomi_sweet

sweet: overlay: Configure aux camera for Aperture
Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

device.mk
@@ -90,7 +90,8 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-evolution \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \

krishna0997x/elixir_device_xiaomi

device.mk
@@ -90,7 +90,8 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-aosp \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \