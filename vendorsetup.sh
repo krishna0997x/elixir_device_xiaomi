82d8ec468b5e38e5820cd533ea85343feed34e98
ugly-kid-af <dwarmachine24@gmail.com>
2023-01-20 19:51:01 +0530

    sweet: Track hardware/lineage/compat
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,9 +2,11 @@
 rm -rf device/xiaomi/sm6150-common
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
+rm -rf hardware/lineage/compat
 
 # clone
 git clone -b 16 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone -b tiramisu https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet.git device/xiaomi/sm6150-common
 git clone -b tiramisu https://github.com/ProjectElixir-Devices/vendor_xiaomi_common_sweet.git vendor/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
