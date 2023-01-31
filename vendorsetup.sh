da7a1ae14365e8b254a1594f0c6e3cb1cac0d38c
ugly-kid-af <dwarmachine24@gmail.com>
2023-01-31 10:02:22 +0000

    sweet: Track Miui Camera
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,9 @@ rm -rf kernel/xiaomi
 rm -rf hardware/lineage/compat
 
 # clone
-git clone -b 16 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
-git clone -b tiramisu https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet.git device/xiaomi/sm6150-common
-git clone -b tiramisu https://github.com/ProjectElixir-Devices/vendor_xiaomi_common_sweet.git vendor/xiaomi
+git clone --depth=1 -b 16 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+git clone --depth=1 -b courbet-13 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 -b tiramisu https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet.git device/xiaomi/sm6150-common
+git clone --depth=1 -b tiramisu https://github.com/ProjectElixir-Devices/vendor_xiaomi_common_sweet.git vendor/xiaomi
 git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
+git clone --depth=1  -b thirteen https://gitlab.com/basamaryan/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
