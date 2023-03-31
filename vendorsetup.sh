3b36652d992edbdc0a3ff8b7d25c66065bd4ffd6
zαι∂ кнαη <kzaid0997@gmail.com>
2023-03-31 16:20:00 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf kernel/xiaomi
 rm -rf hardware/lineage/compat
 
 # clone
-git clone --depth=1 -b 17 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+git clone --depth=1 -b 16 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone --depth=1 -b courbet-13 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone --depth=1 -b tiramisu https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet.git device/xiaomi/sm6150-common
 git clone --depth=1 -b tiramisu https://github.com/ProjectElixir-Devices/vendor_xiaomi_common_sweet.git vendor/xiaomi
